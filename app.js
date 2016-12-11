8a0d341a9c9445c2fd312d913eaa6763ec8e2e6f
Austin Miles <abm@austinbmiles.com>
2016-12-11 12:46:29 -0500

    created variable for signup route and set app to use

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var login = require('./routes/login');
+var signup = require('./routes/signup');
 var users = require('./routes/users');
 
 var app = express();
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/login', login);
+app.use('/signup', signup);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
