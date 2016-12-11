872a03b780dcf4ac645a38b2e489e0762d608a5b
Austin Miles <abm@austinbmiles.com>
2016-12-11 13:47:47 -0500

    added client side validation function for signup form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,4 +3,32 @@ console.log('JS file connected...');
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 
+	$("#signupForm").validate({
+		rules:  {
+
+			signup_email:  {
+				required: true,
+				email: true
+			},
+			signup_password: {
+				required: true
+			},
+			signup_password2:{
+				required: true,
+				equalTo: "#signup_password"
+			}
+		},
+		errorElement : 'div',
+		errorPlacement: function(error, element) {
+			var placement = $(element).data('error');
+			if (placement) {
+				$(placement).append(error)
+			} else {
+				error.insertAfter(element);
+			}
+		}
+
+	});
+
+
 })
