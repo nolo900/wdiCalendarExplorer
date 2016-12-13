1ad511b94e58f77929f47d3c7c246f5a66485e00
Austin Miles <abm@austinbmiles.com>
2016-12-13 09:08:51 -0500

    added fields to closely match Gcal event object

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-let SavedEventSchema = new mongoose.Schema({
+let EventSchema = new mongoose.Schema({
+	gCalEventId: {type: String, required:true},
 	title: {type: String, required: true},
 	dateTime: { type: Date, required: true},
-	comment: {type: String},
-	links: {type: Array},
+	description: { type: String },
+	eventLink: {type: String},
+	extractedlinks: {type: Array},
 
-	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+	//user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 },{
 	timestamps: true
 });
 
-module.exports = mongoose.model('SavedEvent', SavedEventSchema);
+module.exports = mongoose.model('Event', EventSchema);
