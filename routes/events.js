e74f69735af32b085a5ce0958634efc6c3812337
Austin Miles <abm@austinbmiles.com>
2016-12-12 19:40:08 -0500

    added controller.showEvents function to root GET

--- routes/events.js
+++ routes/events.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
-//var controller = require('../controllers/main');
+var controller = require('../controllers/main');
+
+function authenticate(req,res,next) {
+	if(!req.isAuthenticated()) {
+		req.flash('error', 'Please signup or login.');
+		res.redirect('/login');
+	} else {
+		next();
+	}
+}
 
 /* GET /events page. */
-router.get('/', function(req, res, next) {
-	res.send("show my events now!");
+router.get('/', authenticate, function(req, res, next) {
+	//res.send("show my events now!");
+	controller.showEvents(req,res,next);
 });
 
 module.exports = router;
