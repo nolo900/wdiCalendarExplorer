ebecf5e2c5642eef82f073c7565391cf71ef5b78
Austin Miles <abm@austinbmiles.com>
2016-12-12 19:33:57 -0500

    edited root to direct to either event or login depending on user loggedIn status

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@ var controller = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  controller.homePage(req, res);
+  // controller.homePage(req, res);
+  if (req.isAuthenticated()){
+    res.redirect('/events');
+  }  else {
+    res.redirect('/login');
+  }
+
+});
+
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
