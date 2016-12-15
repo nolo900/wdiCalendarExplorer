fd962c834f0b20817eee668a2f8a191ae16100ed
Austin Miles <abm@austinbmiles.com>
2016-12-14 23:39:27 -0500

    added mlab db connection code

--- app.js
+++ app.js
@@ -22,18 +22,18 @@ var app = express();
 
 // Connect to database
 if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-  mongoose.connect('mongodb://localhost/wdiLessonSaver');
+    mongoose.connect('mongodb://localhost/wdiLessonSaver');
 }
 mongoose.connection.on('error', function(err) {
-      console.error('MongoDB connection error: ' + err);
-      process.exit(-1);
+        console.error('MongoDB connection error: ' + err);
+        process.exit(-1);
     }
 );
 mongoose.connection.once('open', function() {
-  console.log("Mongoose has connected to MongoDB!");
+    console.log("Mongoose has connected to MongoDB!");
 });
 
 
